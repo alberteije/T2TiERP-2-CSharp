f4f50c6ee58da1247d2be704452112ecdcd92677
Albert Eije <alberteije@gmail.com>
2019-05-14 23:45:49 +0100

    SearchWindow Fontes

---
+++ libs/SearchWindow/SearchWindow/SearchWindow/Attributes/ColunaDataGrid.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SearchWindow.Attributes
+{
+    [System.AttributeUsage(System.AttributeTargets.Property)]
+    public class ColunaDataGrid : System.Attribute
+    {
+        public bool visivel;
+        public string cabecalho;
+        public int indicePosicao;
+    }
+}
